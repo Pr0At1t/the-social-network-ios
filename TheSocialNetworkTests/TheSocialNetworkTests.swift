6e0cbf1f442c24b6a655f96a5271b84694887006
atulbipin <atulbipin@gmail.com>
2017-11-19 11:17:18 -0800

    Added Quick and Nimble testing syntax for #appReducer

--- TheSocialNetworkTests/TheSocialNetworkTests.swift
+++ TheSocialNetworkTests/TheSocialNetworkTests.swift
@@ -6,24 +6,18 @@
 //  Copyright © 2017 Pr0At1t. All rights reserved.
 //
 
-import XCTest
+import Quick
+import Nimble
+
 @testable import TheSocialNetwork
+@testable import ReSwift
 
-class TheSocialNetworkTests: XCTestCase {
-
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class AppReducerTests: QuickSpec {
+    override func spec() {
+        describe("#appReducer") {
+            it("Initializes state correctly") {
+                expect(true).to(equal(true))
+            }
+        }
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
 }
